b5d38a6f3e63cfd8b6455edbff7188039eb91f75
Mike Sharpe <sharpeprodutcion@gmail.com>
2021-03-02 23:07:13 -0500

    Much more functionality of the js file.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,63 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var charSet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "[", "]", ">", "?", ",", ".", "/"];
-
 // Write password to the #password input
-// for (var i=0; i < charSet.length; i++) {
-//   console.log(charSet[i])
-// }
+var array_of_functions = [getRandomSymbol(), getRandomUpper(), getRandomLower(), getRandomSymbol()];
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+console.log(array_of_functions);
+
+var a = false
+do {
+  var length = prompt("How many characters would you like your password to be?");
+  if (length < 8) {
+    alert("Minimu
+  m length is 8");
+  } else if (length > 128) {
+    alert("Maxmimum length is 128");
+  } else {
+    a = true;
+  }
+}
+while (a === false);
+
+var upper = confirm("Will this contain lower case letters?")
+var lower = confirm("Will this contain upper case letters?")
+var number = confirm("Will this contain numbers?")
+var special = confirm("Will this contain special characters?")
+
+
+function getRandomSymbol() {
+  return String.fromCharCode(Math.floor(Math.random()*15)+33);
 }
 
-console.log(getRandomInt(charSet.length));
-
-for (var i=0; i > document.querySelector("#inputValue"); i++) {
-  console.log(charSet)
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+65);
 }
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random()*26)+97);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random()*10)+48);
+}
+
+function generatePassword() {
+  for (var i=0; i <= length-1; i++) {
+  
+  }
+
+}
+
+
+
 
 
 function writePassword() {
-  var password = generatePassword ();
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
 
 // Reference array of characters to build/ generate a password depending on the length selected.
