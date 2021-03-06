5d4d8f5ff9330cd7bb1b1c2ab3af7da7043e50cc
Mike Sharpe <sharpeprodutcion@gmail.com>
2021-03-06 14:32:15 -0500

    It works, javascript debugged and working as intended

--- js/script.js
+++ js/script.js
@@ -1,27 +1,35 @@
 // Assignment Code
 // Write password to the #password input
-var array_of_functions = [getRandomSymbol(), getRandomUpper(), getRandomLower(), getRandomSymbol()];
+var array_of_functions = [];
 
-console.log(array_of_functions);
+var generateBtn = document.querySelector("#generate");
 
-var a = false
-do {
-  var length = prompt("How many characters would you like your password to be?");
+var length = prompt("How many characters would you like your password to be?");
   if (length < 8) {
-    alert("Minimu
-  m length is 8");
-  } else if (length > 128) {
-    alert("Maxmimum length is 128");
-  } else {
-    a = true;
-  }
-}
-while (a === false);
+  alert("Minimum length is 8");
+} else if (length > 128) {
+  alert("Maxmimum length is 128");
+} else if (isNaN(length)===true) {
+  alert("Unit must be a number")
+} 
+
 
 var upper = confirm("Will this contain lower case letters?")
 var lower = confirm("Will this contain upper case letters?")
 var number = confirm("Will this contain numbers?")
-var special = confirm("Will this contain special characters?")
+var symbol = confirm("Will this contain special characters?")
+
+while (upper === false &&
+  lower === false &&
+  number === false &&
+  symbol === false) 
+  {
+  alert("You must pick at least one option.")
+  upper = confirm("Will this contain lower case letters?")
+  lower = confirm("Will this contain upper case letters?")
+  number = confirm("Will this contain numbers?")
+  symbol = confirm("Will this contain special characters?")
+  }
 
 
 function getRandomSymbol() {
@@ -31,6 +39,7 @@ function getRandomSymbol() {
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random()*26)+65);
 }
+
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random()*26)+97);
 }
@@ -40,17 +49,58 @@ function getRandomNumber() {
 }
 
 function generatePassword() {
-  for (var i=0; i <= length-1; i++) {
-  
+  var password = "";
+
+  if (upper === true){
+    array_of_functions.push(getRandomUpper())
+    password = password +  getRandomUpper();
   }
+  if (lower === true){
+    array_of_functions.push(getRandomLower())
+    password = password + getRandomLower();
+  }
+  if (symbol === true){
+    array_of_functions.push(getRandomSymbol())
+    password = password + getRandomSymbol();
+  }
+  if (number === true){
+    array_of_functions.push(getRandomNumber())
+    password = password + getRandomNumber();
+  }
+  
+  // at tis point we have guaranteed one character from ecvery category choosen by the user
+  // now we must add th reemaining characters by soomehhow randmmly choosing each one
+  // at thhis point, we hve an array oof functions that generate random characters 
+  // only iwthin the categoories set by the user
+  
+  
+  // write the line of code that gets a random character using the array of functions
+  // try this 3 times and see that it works
+  // THEN figure out how to do this a dynamic number of timmes
+  // hint hint it's the reaammining nummber of characters you need to fill
+  // hint hint somoe sort of for loop\
+  
+  
+  // I have an x long password already. For the remainder of the user input length,
+  // I need to cycle from user.input to password.length until password.length = user.input
+
+  if (password.length < length) {
+    for (var i=password.length; i < length; i++) {
+      password += array_of_functions[Math.floor(Math.random()*array_of_functions.length)]
+      
+    }
+  }
+  
+  
+  {
+    return password
+  }
+ 
 
 }
 
-
-
-
-
 function writePassword() {
+ 
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
@@ -61,3 +111,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Reference array of characters to build/ generate a password depending on the length selected.
+
+
+array_of_functions[Math.floor(Math.random()*4)]
+
